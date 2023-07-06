537c3dce86fd5e853b719c1af6d93da50c370aaf
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-06 16:57:55 +0300

    initial commit

---
+++ src/main/java/com/librarium/core/LibrariumCoreApplication.java
@@ -0,0 +1,13 @@
+package com.librarium.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariumCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrariumCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/librarium/core/LibrariumCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarium.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariumCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
