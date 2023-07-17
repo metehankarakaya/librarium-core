d38465b839a242e44b2684200dfdc941e0f8037a
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-18 02:45:38 +0300

    Remove JsonProperty annotation from password field to fix tests

--- src/main/java/com/librarium/core/app/user/model/UserDTO.java
+++ src/main/java/com/librarium/core/app/user/model/UserDTO.java
@@ -1,6 +1,5 @@
 package com.librarium.core.app.user.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -15,7 +14,6 @@ public class UserDTO {
 
     private String username;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private String firstName;
