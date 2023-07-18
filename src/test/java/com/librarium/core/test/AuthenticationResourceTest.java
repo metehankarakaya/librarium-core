2c45e3aa237790610d28f226885f0eb7b1446bd6
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-18 15:26:39 +0300

    Add loginTest to check the login method

--- src/test/java/com/librarium/core/test/AuthenticationResourceTest.java
+++ src/test/java/com/librarium/core/test/AuthenticationResourceTest.java
@@ -1,22 +1,27 @@
 package com.librarium.core.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.librarium.core.app.common.model.LoginDTO;
 import com.librarium.core.app.user.model.UserDTO;
+import com.librarium.core.builder.LoginBuilder;
 import com.librarium.core.builder.UserBuilder;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Order(1)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthenticationResourceTest {
 
     @Autowired
@@ -25,7 +30,11 @@ public class AuthenticationResourceTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
+    @Order(1)
     void registerTest() throws Exception {
 
         final String registerUrl = "/public-app-api/register";
@@ -63,6 +72,31 @@ public class AuthenticationResourceTest {
         Boolean aBoolean2 = objectMapper.readValue(contentAsString2, Boolean.class);
 
         Assertions.assertFalse(aBoolean2);
+    }
+
+    @Test
+    @Order(2)
+    void loginTest() throws Exception {
+
+        final String loginUrl = "/public-app-api/login";
+
+        LoginDTO loginDTO = new LoginBuilder()
+                .buildDefaultLogin()
+                .build();
+
+        ResultActions resultActions = this.mockMvc
+                .perform(
+                        (post(loginUrl))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Accept", "application/json; charset=UTF-8")
+                                .content(objectMapper.writeValueAsBytes(loginDTO)))
+                .andExpect(status().isOk());
+
+        String contentAsString = resultActions.andReturn().getResponse().getContentAsString();
+
+        UserDTO loggedUser = objectMapper.readValue(contentAsString, UserDTO.class);
+
+        Assertions.assertTrue(passwordEncoder.matches(loginDTO.getPassword(), loggedUser.getPassword()));
     }
 
 }
