6fa4954ee38371c4fb062a3d66263bc21ebcd17a
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-28 17:32:36 +0300

    Add setTempId

--- src/main/java/com/librarium/core/app/post/service/PostServiceImpl.java
+++ src/main/java/com/librarium/core/app/post/service/PostServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -67,6 +68,7 @@ public class PostServiceImpl implements PostService {
         User user = getCurrentUser();
 
         Post post = postDTOToPostMapper.map(postDTO);
+        post.setTempId(UUID.randomUUID());
         post.setUser(user);
         if (postDTO.getContent() != null) {
             post.setContent(postDTO.getContent());
